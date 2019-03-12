60f5184ffcc7e083244e9da7c19b625c24aba802
cbtpro <cbtpro@163.com>
2019-03-12 23:52:44 +0800

    add clusterMessage.js

--- web-develop/web/worker/clusterMessage.js
+++ web-develop/web/worker/clusterMessage.js
@@ -1,23 +1,39 @@
 const cluster = require('cluster');
-let arrayLength = 2.5 * Math.pow(10, 7)
-if (cluster.isMaster) {
-  // 分化 worker 对象
-  const coreCount = require('os').cpus().length;
-  for (let i = 0; i < coreCount; i++) { 
-    let worker = cluster.fork(); 
-    worker.send('Hello, Worker!')
-    worker.on('message', function(message) { 
-      if (message._queryId) return; 
-      console.log('work1: ' + work1(arrayLength));
-    });
-  }
 
+let arrayLength = 2.5 * Math.pow(10, 7)
+
+if (cluster.isMaster) {
+  let worker1 = cluster.fork(); 
+  worker1.send('Hello, Worker!')
+  worker1.on('message', function(message) { 
+    if (message._queryId) return;
+    console.log(message)
+    console.log('work1: ' + work1(arrayLength));
+  });
 } else {
   process.send('Hello, main process!');
   process.on('message', function(message) {
+    console.log(message)
+    console.log('work1: ' + work1(arrayLength));
+  });
+}
+
+if (cluster.isMaster) {
+  let worker2 = cluster.fork(); 
+  worker2.send('Hello, Worker!')
+  worker2.on('message', function(message) { 
+    if (message._queryId) return;
+    console.log(message)
+    console.log('work2: ' + work2(arrayLength));
+  });
+} else {
+  process.send('Hello, main process!');
+  process.on('message', function(message) {
+    console.log(message)
     console.log('work2: ' + work2(arrayLength));
   });
 }
+
 function work1(arrLength) {
   let begin = Date.now()
   let arr = new Array(arrLength).fill(0)
